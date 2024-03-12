2d70c84bb45b30576071cf6017d9d7dc1d03f42f
Uwase Sophie <uwasesophie91@gmail.com>
2024-03-13 00:20:44 +0200

    created home screen

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
+import { BottomNav } from './src/components/Navigation';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <>
+      <NavigationContainer>
+        <BottomNav  />
+      </NavigationContainer>
+    </>
   );
 }
 
