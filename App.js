1bfab3b555b8d194a8d4a5f68bb125d2e32e7cc3
Uwase Sophie <uwasesophie91@gmail.com>
2024-03-13 10:25:29 +0200

    .

--- App.js
+++ App.js
@@ -5,10 +5,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { BottomNav } from './src/components/Navigation';
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
     <>
       <NavigationContainer>
         <BottomNav  />
--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text,Dimensions, TouchableOpacity, ScrollView, SafeAreaView, Image } from 'react-native';
 import tw from 'twrnc';
 import Feather from  'react-native-vector-icons/Feather'
+import { Transfer } from '../components/Transfer';
 
 const windowedHeight = Dimensions.get('screen').height
 const width = Dimensions.get('screen')
@@ -10,7 +11,7 @@ const Landing = () => {
     <>
     {/* <View style={{height:50}}></View> */}
     <View style={[tw `bg-[#121214]`, {height:windowedHeight}]}>
-    <View style={[tw `bg-[#202325] pt-5 `, {height:130} ]}>
+    <View style={[tw `border-b border-gray-500 bg-[#202325] pt-8 `, {height:130} ]}>
             <View style={[tw `flex flex-row items-center justify-between p-5 pt-10`]}>
               <View>
                 <Text style={[tw `text-white font-bold text-2xl`]}>Hey, There</Text>
@@ -37,13 +38,41 @@ const Landing = () => {
               <View style={[tw ``, {}]}>
             <TouchableOpacity
               style={[
-                tw ` bg-yellow-500 rounded-lg w-[100%] py-3 px-5`,
-                {},
+                tw ` bg-yellow-500 rounded-lg py-3 px-5`,
+                {width: '80%'}
               ]}
             >
               <Text style={tw `text-black text-xl font-bold`}>Send Money</Text>
             </TouchableOpacity>
           </View>
+            </View>
+            <Transfer  />
+            <View style={[tw `flex p-5 mt-10 m-5 gap-5 border rounded-xl bg-[#202325]`]}>
+              <View>
+                <Text style={[tw `text-white font-bold text-2xl`]}>Helpful tips</Text>
+                <View style={[tw `border-b border-gray-500 my-4`]}></View>
+              </View>
+              <View style={[tw `flex flex-row gap-3 `,{flex: 1}]}>
+                <View style={[tw `bg-[#23C16B] p-5 rounded-xl w-16 h-16`]}>
+                <Feather  
+                  name='percent'
+                  size={23}
+                  color='white'
+                />
+                </View>
+                <View style={[tw `flex-1 `]}>
+                <Text style={[tw `text-white font-bold text-xl`]}>Fee 0%</Text>
+                <Text style={[tw `text-gray-500 `]}>We do not charge fees for transfers with currency conversion. For example, if you are sending in USD but paying for it in EUR.</Text>
+                <TouchableOpacity style={[tw `flex flex-row  gap-2 pt-5`]}>
+                  <Text style={[tw `text-yellow-500`]}>Transfer funds</Text>
+                  <Feather  
+                  name='arrow-right'
+                  size={18}
+                  color='#FCA311'
+                  />
+                </TouchableOpacity>
+                </View>
+              </View>
             </View>
           </ScrollView>
     </View>
