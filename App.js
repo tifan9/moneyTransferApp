b2e0f9287f6bc5f6b9b6355b40c2a269799e3ede
Uwase Sophie <uwasesophie91@gmail.com>
2024-03-15 00:29:49 +0200

    welcome

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { BottomNav, Navigation } from './src/components/Navigation';
@@ -16,11 +17,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/Splash.jsx
+++ src/screens/Splash.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useRef, useState } from 'react';
+import { FlatList, Image, ImageBackground, Pressable, StyleSheet, Text, View,  TouchableOpacity,Dimensions } from 'react-native';
 
-const Splash = () => {
+
+
+const width=Dimensions.get('window').width;
+const height=Dimensions.get('window').height;
+
+
+export default function Splash({navigation}) {
+ 
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+    <TouchableOpacity onPress={()=> navigation.navigate('Welcome')}>
+      <View style={{flex:1,display:'flex', paddingTop:330,alignItems:'center', justifycontent:'center', paddingHorizontal:150}}>
+      <ImageBackground source={require('../../assets/img/splash.png')} style={{width:100,height:100}}/>
+      </View>
+    </TouchableOpacity>
     </View>
   );
 }
 
-export  {Splash}
+
+
+const styles = StyleSheet.create({
+  container: {
+  backgroundColor:'#121214',
+  height:height,
+  width:width
+  },
+});
