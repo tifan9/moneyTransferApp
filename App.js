c70e4142181b9858e5fd88bc3b4fff00eed24205
Uwase Sophie <uwasesophie91@gmail.com>
2024-03-15 00:00:59 +0200

    send money

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { Sendmoney } from './Sendmoney'
-import { Newrecipient } from './Addnewcard'
-import Addnewcard  from './Addnewcard'
+import { Sendmoney } from './src/screens/Sendmoney'
+import { Newrecipient } from './src/screens/Addnewcard'
+import Addnewcard  from './src/screens/Addnewcard'
 
 export default function App() {
   return (
-    // <>
-    //   <Sendmoney />
-    // </>
     <>
-    <Addnewcard/>
+    <Sendmoney />
+    {/* <Addnewcard /> */}
     </>
   )
 }
