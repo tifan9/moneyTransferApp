db9f3ad1a50beaefa3ee9b7b9f72ba21a2ee36e3
Uwase Sophie <uwasesophie91@gmail.com>
2024-03-14 16:47:05 +0200

    History created

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { BottomNav } from './src/components/Navigation';
+import { BottomNav, Navigation } from './src/components/Navigation';
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
       <NavigationContainer>
-        <BottomNav  />
+        <Navigation  />
+        {/* <BottomNav  /> */}
       </NavigationContainer>
       <StatusBar style="light"/>
     </>
