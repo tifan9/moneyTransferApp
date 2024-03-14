555e2a4648db45a57c7fb68d62d147418427192f
Uwase Sophie <uwasesophie91@gmail.com>
2024-03-15 00:00:59 +0200

    send money

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import { StatusBar } from 'expo-status-bar';
 import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 import { View, Text } from 'react-native'
 import React from 'react'
-import { Sendmoney } from './Sendmoney'
-import { Newrecipient } from './Addnewcard'
-import Addnewcard  from './Addnewcard'
+import { Sendmoney } from './src/screens/Sendmoney'
+import { Newrecipient } from './src/screens/Addnewcard'
+import Addnewcard  from './src/screens/Addnewcard'
 
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
