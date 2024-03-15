3f9e1e67d6a053b30d3d4d6992088139748390c6
Rita-anne <annegakiza@gmail.com>
2024-03-15 11:05:40 +0200

    Addnewcards

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Sendmoney } from './src/screens/Sendmoney'
 import { Newrecipient } from './src/screens/Addnewcard'
-import Addnewcard  from './src/screens/Addnewcard'
+import Addnewcard from './src/screens/Addnewcard'
 
 export default function App() {
   return (
     <>
-    <Sendmoney />
-    {/* <Addnewcard /> */}
+    {/* <Sendmoney /> */}
+    <Addnewcard />
     </>
   )
 }
