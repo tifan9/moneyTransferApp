2f98744d1f65a961b1da0fcde7f1e526aa6d382d
Umugwaneza Cynthia <c.umugwanez@alustudent.com>
2024-03-15 09:06:47 +0200

    .

--- App.js
+++ App.js
@@ -9,20 +9,6 @@ const Stack = createNativeStackNavigator();
 export default function App() {
 
   return (
-<<<<<<< HEAD
-<>
-</>
-    // <NavigationContainer>
-    //   <Stack.Navigator initialRouteName='Login'>
-    //     <Stack.Screen name="Login" component={LoginIn} options={{headerShown:false}} />
-    //     <Stack.Screen name="Register" component={Register}  options={{headerShown:false}}/>
-    //     <Stack.Screen name="NewRecipient" component={NewRecipient}  options={{headerShown:false}}/>
-    //     <Stack.Screen name="SenderScreen" component={SenderScreen}  options={{headerShown:false}}/>
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-    
-  );
-=======
     <>
      {/* <NavigationContainer>
        <Stack.Navigator initialRouteName='Login'>
@@ -33,6 +19,5 @@ export default function App() {
    
    </> 
   )
->>>>>>> e0847daa14020b83cd6826c69f4e34d0d3186486
 }
 
