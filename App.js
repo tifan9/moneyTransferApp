355d334730dfa1cc04e6da100ecb949c519421b9
mariaga3 <gahimbaremaria@gmail.com>
2024-03-16 12:09:04 -0700

    hh

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to sxfftardjfldjflt working on yourhjgjhjgjgjg app!</Text>
+      <Text>Open up App.js to sxfftardjfldjflt whhhhhorking on yourhjgjhjgjgjg app!</Text>
       <StatusBar style="auto" />
     </View>
   );
