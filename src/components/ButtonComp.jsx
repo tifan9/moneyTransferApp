9124693f475388b1ad3617939f7c80c963131609
Uwase Sophie <uwasesophie91@gmail.com>
2024-03-17 11:37:12 +0200

    button component added

---
+++ src/components/ButtonComp.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text,StyleSheet, TouchableOpacity} from 'react-native';
+
+const ButtonComp = ({onPress, text1}) => {
+  return (
+    <View style={styles.wholeContainer}>
+      <TouchableOpacity onPress={onPress} style={styles.textContainer}>
+        <Text style={styles.text}> {text1}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+// the styling part of this component 
+
+const styles=StyleSheet.create({
+
+  wholeContainer:{
+    paddingHorizontal:25,
+    paddingTop:20
+  },
+  textContainer:{
+    backgroundColor:'#FCA210',
+    borderRadius:8,
+  },
+
+  text:{
+    fontSize:17,
+    paddingVertical:10,
+    textAlign:'center',
+    color:'#121315',
+  }
+})
+
+export {ButtonComp}
