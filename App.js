b9d2993f1f25922f20c8c32ca81dcd64a01d53d6
Uwase Sophie <uwasesophie91@gmail.com>
2024-03-18 12:40:34 +0200

    test

--- App.js
+++ App.js
@@ -6,11 +6,7 @@ import Register from './src/screens/Register';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { BottomNav, Navigation } from './src/components/Navigation';
-<<<<<<< HEAD
 import DropdownInput from './src/screens/DropdownInput';
-=======
-
->>>>>>> 7b3ca5b5df0bb9007fd23fe70e1000969513e525
 const Stack = createNativeStackNavigator();
 export default function App() {
 
